804ac828f2115571536d3c7387feb07b7daef14a
Abhimanyu Singh Rathore <abhir95@gmail.com>
2021-11-30 13:26:02 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -7,5 +7,5 @@ config.apiDomain = process.env.apiDomain || "devapi.lrinternal.com";
 config.apiKey = process.env.key || "5476ffe6-6f76-4897-b597-b339ac57ea79";
 config.apiSecret =  process.env.secret || "57265328-9777-4fe9-b33d-fc6762feb260";
 config.idxDomain = 'https://b2b-demo.devhub.lrinternal.com/auth';
-config.apiEndpoint = 'http://15.206.240.193:3001';
+config.apiEndpoint = 'https://b2blrdemo.herokuapp.com';
 module.exports = config;
